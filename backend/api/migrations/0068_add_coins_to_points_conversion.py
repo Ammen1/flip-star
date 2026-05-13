475fcc176878e4b6fc8c26fb08c475d2f691d41e
derartuge19 <derartuge@gmail.com>
2026-05-13 11:47:28 +0300

    Fix migration dependency from 0067 to 0066

--- backend/api/migrations/0068_add_coins_to_points_conversion.py
+++ backend/api/migrations/0068_add_coins_to_points_conversion.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0067_block_onevaschargingtransaction_and_more'),
+        ('api', '0066_add_gifts_weight_to_scoring_config'),
     ]
 
     operations = [
