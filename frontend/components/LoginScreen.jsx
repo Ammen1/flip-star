551f472da6d7fb05cf8d3dd2bf78dd176fd8852a
derartuge19 <derartuge@gmail.com>
2026-05-14 17:00:13 +0300

    Fix image path to use backend/static/images folder for production

--- frontend/components/LoginScreen.jsx
+++ frontend/components/LoginScreen.jsx
@@ -75,7 +75,7 @@ export function LoginScreen({ onSuccess, onRegister, onBack }) {
           alignItems: "center", 
           justifyContent: "center"
         }}>
-          <img src="/assets/70x20.png" alt="Logo" style={{ width: 140, height: 40, objectFit: "contain" }} />
+          <img src="/static/images/70x20.png" alt="Logo" style={{ width: 140, height: 40, objectFit: "contain" }} />
         </div>
       </div>
       <div style={{ flex:1, background:"#fff", borderRadius:"28px 28px 0 0", marginTop:30, padding:"28px 24px 40px" }}>
