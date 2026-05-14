7d26215fe97418eb387861dbac0e4e2f02c21c50
derartuge19 <derartuge@gmail.com>
2026-05-14 17:04:53 +0300

    Fix image path to use frontend/public/assets for nginx to serve

--- frontend/components/ModernLoginScreen.jsx
+++ frontend/components/ModernLoginScreen.jsx
@@ -537,7 +537,7 @@ export function ModernLoginScreen({ onSuccess, onRegister, onBack }) {
           alignItems: "center", 
           justifyContent: "center"
         }}>
-          <img src="/static/images/70x20.png" alt="Logo" style={{ width: 140, height: 40, objectFit: "contain" }} />
+          <img src="/assets/70x20.png" alt="Logo" style={{ width: 140, height: 40, objectFit: "contain" }} />
         </div>
 
         {/* Card */}
