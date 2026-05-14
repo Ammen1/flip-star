ebc904d3f920e4cce44f5bd43e075bf949a6c297
derartuge19 <derartuge@gmail.com>
2026-05-14 20:19:53 +0300

    Apply split background logo to all registration pages

--- frontend/components/ModernRegisterScreen.jsx
+++ frontend/components/ModernRegisterScreen.jsx
@@ -242,15 +242,17 @@ export function ModernRegisterScreen({ onSuccess, onLogin, onBack }) {
 
         {/* Logo Header */}
         <div style={{ 
-          background: "#000000", 
           height: 90, 
           marginBottom: 24, 
           borderRadius: 12, 
           display: "flex", 
-          alignItems: "center", 
-          justifyContent: "center"
+          flexDirection: "row",
+          overflow: "hidden",
+          position: "relative"
         }}>
-          <img src="/assets/logo-ethio-and-flip.png" alt="Logo" style={{ width: 420, height: 90, objectFit: "contain" }} />
+          <div style={{ width: "50%", backgroundColor: "#FFFFFF", height: "100%", position: "absolute", left: 0, top: 0 }}></div>
+          <div style={{ width: "50%", backgroundColor: "#000000", height: "100%", position: "absolute", right: 0, top: 0 }}></div>
+          <img src="/assets/logo-ethio-and-flip.png" alt="Logo" style={{ height: 90, width: "100%", objectFit: "contain", position: "relative", zIndex: 1 }} />
         </div>
 
         {/* Step indicator */}
