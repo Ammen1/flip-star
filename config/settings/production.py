efbd7c241413b1da7b7dc3dc03973ca60c7dc591
Ethan313241 <amenguda@gmail.com>
2026-08-26 00:00:23 +0300

    fix now on for loop

--- config/settings/production.py
+++ config/settings/production.py
@@ -128,7 +128,9 @@ SECURE_REFERRER_POLICY = 'same-origin'
 X_FRAME_OPTIONS = 'DENY'
 
 CSRF_TRUSTED_ORIGINS = [
-    o.strip() for o in config('CSRF_TRUSTED_ORIGINS', default='https://api.uat.flipstar.et').split(',') if o.strip()
+    o.strip()
+    for o in config('CSRF_TRUSTED_ORIGINS', default='https://api.uat.flipstar.et').split(',')
+    if o.strip()
 ]
 
 
